5190192029bab3426b9803369283bfc41e2126a3
mounika <root@ip-172-31-7-32.us-west-1.compute.internal>
2020-05-17 04:33:15 +0000

    Docker Install Script

--- setup_scripts/docker-install.sh
+++ setup_scripts/docker-install.sh
@@ -17,5 +17,8 @@ sudo apt-get -y install docker.io
 # add current user to docker group so there is no need to use sudo when running docker
 sudo usermod -aG docker $(whoami)
 
+
+#You would need to loog out and log back in so that your group membership is re-evaluated or type the following command:
+
 echo "Docker Installed"
 docker --version
