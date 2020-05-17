5faa3dd1279c6c7b2c4ecd57966a68b522265a14
mounika <root@ip-172-31-7-32.us-west-1.compute.internal>
2020-05-17 04:30:21 +0000

    Docker Install Script

---
+++ setup_scripts/docker-install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common curl -fsSL https://yum.dockerproject.org/gpg | sudo apt-key add - 
+    
+sudo add-apt-repository \
+    "deb https://apt.dockerproject.org/repo/ \
+    ubuntu-$(lsb_release -cs) \
+    main"
+
+sudo apt-get update
+sudo apt-get -y install docker.io
+# add current user to docker group so there is no need to use sudo when running docker
+sudo usermod -aG docker $(whoami)
+
+echo "Docker Installed"
+docker --version
