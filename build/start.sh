2628456e5ab6872e0cfbf90e25fd6ed2ef72f140
Krishna Venkatachalam <krishna.v@rahisystems.com>
2020-05-17 21:54:19 +0000

    service edit

--- build/start.sh
+++ build/start.sh
@@ -1,13 +1,6 @@
 #!/bin/sh
-#set -e
+set -e
 
-#Update the /etc/hosts
-echo "Radius Host $RADIUS_HOST"
-echo "Radius HostNAME $RADIUS_HOSTNAME"
-echo "$RADIUS_HOST  $RADIUS_HOSTNAME" >> /etc/hosts
-
-#v alidate the DNS Resolve
-#ping $RADIUS_HOSTNAME -c 1
 
 # Start Radsecproxy
 /usr/local/sbin/radsecproxy -d 5 -f -i /var/run/radsecproxy.pid
