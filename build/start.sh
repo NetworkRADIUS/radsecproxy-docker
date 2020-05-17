7e4b854b6b2fd0e0097a2713ddc69f8f4cd0f1c8
Krishna Venkatachalam <krishna.v@rahisystems.com>
2020-05-17 05:18:58 +0000

    start.sh edited online with Bitbucket

--- build/start.sh
+++ build/start.sh
@@ -4,10 +4,10 @@
 #Update the /etc/hosts
 echo "Radius Host $RADIUS_HOST"
 echo "Radius HostNAME $RADIUS_HOSTNAME"
-echo "$RADIUS_IP  $RADIUS_HOSTNAME" >> /etc/hosts
+echo "$RADIUS_HOST  $RADIUS_HOSTNAME" >> /etc/hosts
 
 #v alidate the DNS Resolve
-ping $RADIUS_HOSTNAME -c 1
+#ping $RADIUS_HOSTNAME -c 1
 
 # Start Radsecproxy
 /usr/local/sbin/radsecproxy -d 5 -f -i /var/run/radsecproxy.pid
