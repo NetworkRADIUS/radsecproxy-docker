b4c9d57efa33463be84729011462631c176e1710
Ubuntu <ubuntu@ip-172-31-3-93.us-west-2.compute.internal>
2020-05-21 04:15:40 +0000

    Multi-stage Build

---
+++ setup_scripts/build-container.sh
@@ -0,0 +1,31 @@
+echo "Creating the Container"
+
+#create the container
+sudo docker create \
+    -p $AUTH_PORT:1812/udp -p $ACCOUNTING_PORT:1813/udp \
+    --name $RADSECPROXY_SERVER_NAME \
+    --restart always \
+    cloudteamrahi48303/radsecproxy:latest
+
+echo "Coping key,certificates and config files to the Container"
+
+#Copy the key ,Certificates  and the radsecproxy config file
+sudo docker cp $RADSEC_KEY_FILE $RADSECPROXY_SERVER_NAME:/etc/ssl/certs/radsec.key.pem
+sudo docker cp $RADSEC_CRT_FILE $RADSECPROXY_SERVER_NAME:/etc/ssl/certs/radsec.crt.pem
+sudo docker cp $CA_CERT_FILE $RADSECPROXY_SERVER_NAME:/etc/ssl/certs/cacert.pem
+sudo docker cp $RADSEC_CONFIG_FILE $RADSECPROXY_SERVER_NAME:/usr/local/etc/radsecproxy.conf
+
+echo "copy completed!"
+
+echo "Starting the Radsecproxy Container"
+#Start the Container 
+sudo docker start $RADSECPROXY_SERVER_NAME
+
+#Wait for 3 secs to start the container to get the complete logs
+sleep 3  # Waits 3 seconds.
+
+
+#display the logs of the container
+echo "RadsecProxy Server Log:"
+sudo docker logs $RADSECPROXY_SERVER_NAME
+
---
+++ setup_scripts/cleanup-script.sh
@@ -0,0 +1,31 @@
+#Clean up script
+echo "Cleaning all the files/directories installed for the Radsec Proxy deployment"
+
+#if exists remove the certs directory
+CERTS_DIR=$HOME/certs/
+
+if [ -d "$CERTS_DIR" ] 
+then
+    sudo rm -fr $CERTS_DIR
+fi
+echo "..erased all cert related artifacts( $CERTS_DIR )" 
+
+
+
+#if exists remove the radsecproxy directory
+
+RADSECPROXY_DIR=$HOME/radsecproxy/
+
+if [ -d "$RADSECPROXY_DIR" ] 
+then
+    sudo rm -fr $RADSECPROXY_DIR
+fi
+echo "..erased git downloads ( $RADSECPROXY_DIR )" 
+
+
+#Purge the installed packages as part of setup
+sudo apt-get purge git unzip
+echo "..purged all installed packages( git and unzip )"
+
+
+echo "Cleaning done"
