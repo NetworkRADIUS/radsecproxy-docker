987505eb8438e0ebd913fd43fb64e65d87031bd3
Mounika Alla <mounika.a@rahisystems.com>
2020-05-27 10:00:20 +0000

    setup.sh edited online with Bitbucket

--- setup_scripts/setup.sh
+++ setup_scripts/setup.sh
@@ -14,6 +14,7 @@ sudo add-apt-repository \
     main"
 
 sudo apt-get update
+sudo apt-get -y install tree
 sudo apt-get -y install docker.io
 # add current user to docker group so there is no need to use sudo when running docker
 sudo usermod -aG docker $(whoami)
