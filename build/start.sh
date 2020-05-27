b9bffd5eceef6f06acf80b652e4b3959250b5dec
Krishna Venkatachalam <krishna.v@rahisystems.com>
2020-05-27 09:37:41 +0000

    start.sh edited online with Bitbucket

--- build/start.sh
+++ build/start.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -e
 
-update-ca-certificates
+#update-ca-certificates
 # Start Radsecproxy
-/usr/local/sbin/radsecproxy -d 5 -f -i /var/run/radsecproxy.pid
+#/usr/local/sbin/radsecproxy -d 5 -f -i /var/run/radsecproxy.pid
+/sbin/radsecproxy -f -c /etc/radsecproxy.conf -i /var/run/radsecproxy.pid
+
