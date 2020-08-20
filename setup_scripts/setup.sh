0de96a3cbf067a0ad0481c26f5cb1a8e9da9b1a2
Network RADIUS <support@networkradius.com>
2020-08-20 17:08:06 +0100

    Fix the installation of Docker from the official repository
    The Docker repository that was being configured was abandoned.
    
    Docker from the Linux distribution was being used unintentionally rather
    than Docker-CE.

--- setup_scripts/setup.sh
+++ setup_scripts/setup.sh
@@ -6,16 +6,19 @@ sudo apt-get install -y \
     ca-certificates \
     curl \
     software-properties-common 
-curl -fsSL https://yum.dockerproject.org/gpg | sudo apt-key add - 
-    
+
+
+# Install Docker from the official repo
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
 sudo add-apt-repository \
-    "deb https://apt.dockerproject.org/repo/ \
-    ubuntu-$(lsb_release -cs) \
-    main"
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
 
 sudo apt-get update
 sudo apt-get -y install tree
-sudo apt-get -y install docker.io
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io
 # add current user to docker group so there is no need to use sudo when running docker
 sudo usermod -aG docker $(whoami)
 
