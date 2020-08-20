3a4468a789a5cb4b51f48eac44fc3543e7b64594
Network RADIUS <support@networkradius.com>
2020-08-20 17:08:32 +0100

    Use a proper init system rather than a shell
    If we want to have reliable container restarts then we need to have PID 1 and
    the RadSec process handle signals correctly.

--- build/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -e
-
-#update-ca-certificates
-# Start Radsecproxy
-#/usr/local/sbin/radsecproxy -d 5 -f -i /var/run/radsecproxy.pid
-/sbin/radsecproxy -f -c /etc/radsecproxy.conf -i /var/run/radsecproxy.pid
-
